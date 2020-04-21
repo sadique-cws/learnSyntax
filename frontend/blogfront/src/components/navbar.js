c66588f98edc05e3cc4da67d0b36e3a4c5c314b5
sadique-cws <32757358+sadique-cws@users.noreply.github.com>
2020-04-21 14:39:25 +0530

    Initial commit

---
+++ frontend/blogfront/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div className="navbar navbar-expand-lg navbar-dark bg-danger">
+                <a className="navbar-brand">Blog</a>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
